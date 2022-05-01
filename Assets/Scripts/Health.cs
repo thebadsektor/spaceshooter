966ef451ed03543ea0040fa60c47e3242b649cb5
badsektor <gerald.villaran@lspu.edu.ph>
2022-05-02 01:58:27 +0800

    added scorekeeper

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] bool isPlayer;
     [SerializeField] int health = 50;
+    [SerializeField] int score = 50;
     [SerializeField] ParticleSystem hitEffect;
 
     [SerializeField] bool applyCameraShake;
@@ -12,10 +14,13 @@ public class Health : MonoBehaviour
 
     AudioPlayer audioPlayer;
 
+    ScoreKeeper scoreKeeper;
+
     void Awake()
     {
         cameraShake = Camera.main.GetComponent<CameraShake>();
         audioPlayer = FindObjectOfType<AudioPlayer>();
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -31,14 +36,28 @@ public class Health : MonoBehaviour
             damageDealer.Hit();
         }
 
+        int GetHealth()
+        {
+            return health;
+        }
+
         void TakeDamage(int damage)
         {           
 
             health -= damage;
             if(health <= 0)
             {
-                Destroy(gameObject);
+                Die();
             }
+        }
+
+        void Die()
+        {
+            if(!isPlayer)
+            {
+                scoreKeeper.ModifyScore(score);
+            }
+            Destroy(gameObject);
         }
 
         void PlayHitEffect()
