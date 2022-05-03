776820242a5eaba440b10a3f7db51c04fdc2ae81
badsektor <gerald.villaran@lspu.edu.ph>
2022-05-04 00:04:26 +0800

    added UIGameOver script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -57,7 +57,7 @@ public class Health : MonoBehaviour
             }
             else
             {
-                levelManager.LoadGameOver(scoreKeeper.GetScore());
+                levelManager.LoadGameOver();
             }
             Destroy(gameObject);
         }
